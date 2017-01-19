442c42cec49410200c0b1a94d4146fb27339ac2a
Paul Ambrose <pambrose@mac.com>
2017-01-19 14:59:07 -0500

    Update keyboard_controller

--- keyboard_controller.py
+++ keyboard_controller.py
@@ -20,55 +20,6 @@ RIGHT = "RIGHT"
 FORWARD = "FORWARD"
 BACKWARD = "BACKWARD"
 
-
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code: {0}".format(rc))
-
-
-def on_disconnect(client, userdata, rc):
-    print("Disconnected with result code: {0}".format(rc))
-
-
-def on_publish(client, userdata, mid):
-    print("Published value to {0} with message id {1}".format(COMMAND, mid))
-
-
-def publish_command(cmd):
-    result, mid = mqtt_conn.client.publish(COMMAND, payload=cmd.encode('utf-8'))
-
-
-def on_left_arrow_pressed(event):
-    label["text"] = LEFT
-    publish_command(LEFT)
-
-
-def on_right_arrow_pressed(event):
-    label["text"] = RIGHT
-    publish_command(RIGHT)
-
-
-def on_up_arrow_pressed(event):
-    label["text"] = FORWARD
-    publish_command(FORWARD)
-
-
-def on_down_arrow_pressed(event):
-    label["text"] = BACKWARD
-    publish_command(BACKWARD)
-
-
-def key(event):
-    key_clicked = repr(event.char)
-    label["text"] = "Pressed {0}".format(key_clicked)
-    if key_clicked == "'q'":
-        sys.exit()
-
-
-def on_mouseclick(event):
-    root.focus_set()
-    label["text"] = "Clicked at {0},{1}".format(event.x, event.y)
-
-
 if __name__ == "__main__":
     # Parse CLI args
     parser = argparse.ArgumentParser()
@@ -78,10 +29,25 @@ if __name__ == "__main__":
     # Setup logging
     logging.basicConfig(stream=sys.stderr, level=logging.INFO, format=FORMAT_DEFAULT)
 
+    ##### Setup MQTT #####
+
     # Create MQTT connection
     mqtt_conn = MqttConnection(*mqtt_broker_info(args["mqtt"]))
 
+
     # Setup MQTT callbacks
+    def on_connect(client, userdata, flags, rc):
+        print("Connected with result code: {0}".format(rc))
+
+
+    def on_disconnect(client, userdata, rc):
+        print("Disconnected with result code: {0}".format(rc))
+
+
+    def on_publish(client, userdata, mid):
+        print("Published value to {0} with message id {1}".format(COMMAND, mid))
+
+
     mqtt_conn.client.on_connect = on_connect
     mqtt_conn.client.on_disconnect = on_disconnect
     mqtt_conn.client.on_publish = on_publish
@@ -89,10 +55,49 @@ if __name__ == "__main__":
     # This will not block
     mqtt_conn.connect()
 
+
+    ##### Setup tkinter #####
+
+    def publish(cmd):
+        result, mid = mqtt_conn.client.publish(COMMAND, payload=cmd.encode('utf-8'))
+
+
+    def on_left_arrow_pressed(event):
+        label["text"] = LEFT
+        publish(LEFT)
+
+
+    def on_right_arrow_pressed(event):
+        label["text"] = RIGHT
+        publish(RIGHT)
+
+
+    def on_up_arrow_pressed(event):
+        label["text"] = FORWARD
+        publish(FORWARD)
+
+
+    def on_down_arrow_pressed(event):
+        label["text"] = BACKWARD
+        publish(BACKWARD)
+
+
+    def on_key(event):
+        key_clicked = repr(event.char)
+        label["text"] = "Pressed {0}".format(key_clicked)
+        if key_clicked == "'q'":
+            sys.exit()
+
+
+    def on_mouseclick(event):
+        root.focus_set()
+        label["text"] = "Clicked at {0},{1}".format(event.x, event.y)
+
+
     root = tk.Tk()
     # For bind() details, see: http://effbot.org/tkinterbook/tkinter-events-and-bindings.htm
     root.bind("<Button-1>", on_mouseclick)
-    root.bind("<Key>", key)
+    root.bind("<Key>", on_key)
     root.bind('<Left>', on_left_arrow_pressed)
     root.bind('<Right>', on_right_arrow_pressed)
     root.bind('<Up>', on_up_arrow_pressed)
