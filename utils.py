19832c2006435ee72f9eb2cd910b4a402127844a
Paul Ambrose <pambrose@mac.com>
2017-01-20 16:34:40 -0500

    Update docs

--- utils.py
+++ utils.py
@@ -7,6 +7,7 @@ FORMAT_DEFAULT = "%(asctime)s %(name)-10s %(funcName)-10s():%(lineno)i: %(leveln
 
 
 def mqtt_broker_info(val):
+    # Broker hostname can be either "localhost" or "localhost:999"
     return (val[:val.index(":")], int(val[val.index(":") + 1:])) if ":" in val else (val, 1883)
 
 
